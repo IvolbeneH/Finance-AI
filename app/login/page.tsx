c0e38dd8120b32a1b21445e04f076671cb76ef6d
IvolbeneH <ivolbene1@gmail.com>
2024-11-05 15:21:41 -0300

    feat: add a clerk authenticate in application

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2 bg-[#0F0E11]">
       {/* ESQUERDA */}
-      <div className="mx-auto flex h-full max-w-[650px] flex-col justify-center p-8">
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
           src="/logo.svg"
           alt="Finance AI"
@@ -15,19 +24,17 @@ export default function LoginPage() {
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground mb-8 text-lg">
+        <p className="text-muted-foreground mb-8">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button
-          variant="outline"
-          size="lg"
-          className="mr-2 h-11 bg-[#0F0E11] text-base"
-        >
-          <LogInIcon />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline" size="lg" className="mr-2 bg-[#0F0E11]">
+            <LogInIcon />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/* DIREITA */}
       <div className="relative h-full w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-    <div>
-      <h1>Initial page</h1>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
     </div>
   );
 }
